c620808057e7a345ead798bb1dcb4e74cce30e32
Abinash1610 <abinashbiswal65@gmail.com>
2022-12-01 20:05:32 +0530

    commit69

--- src/components/HomePage/HomeCard.js
+++ src/components/HomePage/HomeCard.js
@@ -1,14 +1,23 @@
 import React from 'react'
 import './HomeCard.css'
+import feve1 from './formal event1.jpg';
+import feve2 from './formal event2.jpg';
+import feve3 from './formal event3.jpg';
+import feve4 from './formal event4.jpg';
+import feve5 from './formal event5.jpg';
+import feve6 from './formal event6.jpg';
+import feve7 from './formal event7.jpg';
+import feve8 from './formal event8.jpg';
+
 export default function HomeCard() {
   return (
     <div class="BM2container">
-        <div className='BM2_Title'>Latest Blogs</div>
+        <div className='BM2_Title'>EVENTS & MEETS</div>
         <div class="row">
             <div class="col-md-4">
                 <div class="card-content">
                     <div class="card-img">
-                        <img src="https://placeimg.com/380/230/nature" alt=""/>
+                        <img src={feve1} alt=""/>
                     </div>
                     <div class="card-desc">
                         <h3>Heading1</h3>
@@ -21,7 +30,7 @@ export default function HomeCard() {
             <div class="col-md-4">
                 <div class="card-content">
                     <div class="card-img">
-                        <img src="https://placeimg.com/380/230/nature" alt=""/>
+                        <img src={feve2} alt=""/>
                     </div>
                     <div class="card-desc">
                         <h3>Heading1</h3>
@@ -34,7 +43,7 @@ export default function HomeCard() {
             <div class="col-md-4">
                 <div class="card-content">
                     <div class="card-img">
-                        <img src="https://placeimg.com/380/230/nature" alt=""/>
+                        <img src={feve3} alt=""/>
                     </div>
                     <div class="card-desc">
                         <h3>Heading1</h3>
@@ -47,7 +56,7 @@ export default function HomeCard() {
             <div class="col-md-4">
                 <div class="card-content">
                     <div class="card-img">
-                        <img src="https://placeimg.com/380/230/nature" alt=""/>
+                        <img src={feve4} alt=""/>
                     </div>
                     <div class="card-desc">
                         <h3>Heading1</h3>
@@ -60,7 +69,7 @@ export default function HomeCard() {
             <div class="col-md-4">
                 <div class="card-content">
                     <div class="card-img">
-                        <img src="https://placeimg.com/380/230/animals" alt=""/>
+                        <img src={feve5} alt=""/>
                     </div>
                     <div class="card-desc">
                         <h3>Heading2</h3>
@@ -73,7 +82,7 @@ export default function HomeCard() {
             <div class="col-md-4">
                 <div class="card-content">
                     <div class="card-img">
-                        <img src="https://placeimg.com/380/230/animals" alt=""/>
+                        <img src={feve6} alt=""/>
                     </div>
                     <div class="card-desc">
                         <h3>Heading2</h3>
@@ -86,7 +95,7 @@ export default function HomeCard() {
             <div class="col-md-4">
                 <div class="card-content">
                     <div class="card-img">
-                        <img src="https://placeimg.com/380/230/animals" alt=""/>
+                        <img src={feve7} alt=""/>
                     </div>
                     <div class="card-desc">
                         <h3>Heading2</h3>
@@ -99,23 +108,10 @@ export default function HomeCard() {
             <div class="col-md-4">
                 <div class="card-content">
                     <div class="card-img">
-                        <img src="https://placeimg.com/380/230/animals" alt=""/>
+                        <img src={feve8} alt=""/>
                     </div>
                     <div class="card-desc">
                         <h3>Heading2</h3>
-                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam, voluptatum! Dolor quo, perspiciatis
-                            voluptas totam</p>
-                            <a href="/" class="btn-card">Read</a>   
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-4">
-                <div class="card-content">
-                    <div class="card-img">
-                        <img src="https://placeimg.com/380/230/tech" alt=""/>
-                    </div>
-                    <div class="card-desc">
-                        <h3>Heading3</h3>
                         <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam, voluptatum! Dolor quo, perspiciatis
                             voluptas totam</p>
                             <a href="/" class="btn-card">Read</a>   
