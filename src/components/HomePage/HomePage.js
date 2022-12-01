f263c739673ef9cc932a6ea75bc77b1b1f931428
Sai Nanda <saisoumyaknanda@gmail.com>
2022-12-01 18:17:01 +0530

    Home

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,10 +1,12 @@
 import React from "react";
+import HomeCard from "./HomeCard";
 import "./HomePage.css";
 import Slides from "./Slides";
 export default function HomePage() {
   return (
     <div className="HomePage">
        <Slides/>
+       <HomeCard/>
     </div>
   );
 }
